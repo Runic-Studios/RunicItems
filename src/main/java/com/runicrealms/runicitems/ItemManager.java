ae1201e085aff33164be534adccb65e8fbf0cc99
Excel619 <kai@enoshlo.com>
2020-12-24 16:17:42 -0700

    Unfinished changes

--- src/main/java/com/runicrealms/runicitems/ItemManager.java
+++ src/main/java/com/runicrealms/runicitems/ItemManager.java
@@ -5,23 +5,23 @@ import com.runicrealms.plugin.character.api.CharacterQuitEvent;
 import com.runicrealms.plugin.database.Data;
 import com.runicrealms.runicitems.config.ItemLoader;
 import com.runicrealms.runicitems.item.RunicItem;
+import com.runicrealms.runicitems.item.util.ItemNbtUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ItemManager implements Listener {
 
-    private static Map<Integer, RunicItem> cachedItems = new HashMap<Integer, RunicItem>();
-    private static Map<Player, Map<Integer, RunicItem>> cachedPlayerItems = new HashMap<Player, Map<Integer, RunicItem>>();
+    private static Map<Long, RunicItem> cachedItems = new HashMap<Integer, RunicItem>();
 
     @EventHandler
     public void onCharacterJoin(CharacterLoadEvent event) {
         if (Plugin.isDatabaseLoadingEnabled()) {
-            loadPlayerItems(event.getPlayerCache().getMongoData().getSection("character." + event.getSlot() + ".inventory"), event.getPlayer());
+            loadItems(event.getPlayerCache().getMongoData().getSection("character." + event.getSlot() + ".inventory"));
         }
     }
 
@@ -29,31 +29,28 @@ public class ItemManager implements Listener {
     public void onCharacterQuit(CharacterQuitEvent event) {
         if (Plugin.isDatabaseLoadingEnabled()) {
             Bukkit.getScheduler().runTaskAsynchronously(Plugin.getInstance(), () -> {
-                // TODO - save player inventory
-                for (Integer key : cachedPlayerItems.get(event.getPlayer()).keySet()) {
-                    cachedItems.remove(key);
+                for (ItemStack itemStack : event.getPlayer().getInventory()) {
+                    event.getPlayerCache().getMongoData().
                 }
-                cachedPlayerItems.remove(event.getPlayer());
             });
         }
     }
 
-    public static void loadPlayerItems(Data data, Player player) { // Player, Bank
-        Map<Integer, RunicItem> playerItems = new HashMap<Integer, RunicItem>();
+    public static void loadItems(Data data) { // Guild Bank, Trade Market
         for (String key : data.getKeys()) {
-            playerItems.put(Integer.parseInt(key), ItemLoader.loadItem(data.getSection(key)));
-        }
-        cachedPlayerItems.put(player, playerItems);
-    }
-
-    public static void loadGlobalItems(Data data) { // Guild Bank, Trade Market
-        for (String key : data.getKeys()) {
-            cachedItems.put(Integer.parseInt(key), ItemLoader.loadItem(data.getSection(key)));
+            cachedItems.put(Long.parseLong(key), ItemLoader.loadItem(data.getSection(key)));
         }
     }
 
     public RunicItem getItemFromId(Integer id) {
         return cachedItems.get(id);
+    }
+
+    public RunicItem getItemFromItemStack(ItemStack itemStack) {
+        if (ItemNbtUtils.hasNbtLong(itemStack, "runic-item-id")) {
+            return cachedItems.get(ItemNbtUtils.getNbtLong(itemStack, "runic-item-id"));
+        }
+        return null;
     }
 
 }
