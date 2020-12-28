34f4a6fb870eff50677acc5518646480b9aa0fc3
Excel619 <kai@enoshlo.com>
2020-12-28 12:29:25 -0700

    Updated to use CacheSaveEvent instead of CharacterQuitEvent

--- src/main/java/com/runicrealms/runicitems/ItemManager.java
+++ src/main/java/com/runicrealms/runicitems/ItemManager.java
@@ -1,8 +1,10 @@
 package com.runicrealms.runicitems;
 
+import com.runicrealms.plugin.api.RunicCoreAPI;
 import com.runicrealms.plugin.character.api.CharacterLoadEvent;
 import com.runicrealms.plugin.character.api.CharacterQuitEvent;
 import com.runicrealms.plugin.database.Data;
+import com.runicrealms.plugin.database.event.CacheSaveEvent;
 import com.runicrealms.runicitems.config.ItemLoader;
 import com.runicrealms.runicitems.item.RunicItem;
 import com.runicrealms.runicitems.item.util.ItemNbtUtils;
@@ -25,25 +27,22 @@ public class ItemManager implements Listener {
         }
     }
 
-    // TODO - switch to new runic core event
     @EventHandler
-    public void onCharacterQuit(CharacterQuitEvent event) {
+    public void onCacheSave(CacheSaveEvent event) {
         if (Plugin.isDatabaseLoadingEnabled()) {
-            Bukkit.getScheduler().runTaskAsynchronously(Plugin.getInstance(), () -> {
-                ItemStack[] contents = event.getPlayer().getInventory().getContents();
-                for (int i = 0; i < contents.length; i++) {
-                    RunicItem runicItem = getItemFromItemStack(contents[i]);
-                    if (runicItem != null) {
-                        runicItem.addToData(event.getPlayerCache().getMongoData().getSection("character." + event.getSlot() + ".inventory." + i));
-                    }
+            ItemStack[] contents = event.getPlayer().getInventory().getContents();
+            for (int i = 0; i < contents.length; i++) {
+                RunicItem runicItem = getItemFromItemStack(contents[i]);
+                if (runicItem != null) {
+                    runicItem.addToData(event.getMongoDataSection().getSection("inventory." + i));
                 }
-            });
+            }
         }
     }
 
-    public static void loadItems(Data data) { // Guild Bank, Trade Market
+    public static void loadItems(Data data) {
         for (String key : data.getKeys()) {
-            RunicItem loadedItem = ItemLoader.loadItem(data.getSection(key)); // TODO - load id, itemOwner
+            RunicItem loadedItem = ItemLoader.loadItem(data.getSection(key));
             cachedItems.put(loadedItem.getId(), loadedItem);
         }
     }
