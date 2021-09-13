43c0809b6b7298d25c1a88efd25829c2a6aa3834
Excel619 <54898677+Excel619@users.noreply.github.com>
2021-09-13 13:45:00 -0600

    Fixed loot not distributing between armor and weapons correctly (80-20)

--- src/main/java/com/runicrealms/runicitems/LootManager.java
+++ src/main/java/com/runicrealms/runicitems/LootManager.java
@@ -1,15 +1,21 @@
 package com.runicrealms.runicitems;
 
 import com.runicrealms.runicitems.item.stats.RunicItemRarity;
+import com.runicrealms.runicitems.item.template.RunicItemArmorTemplate;
 import com.runicrealms.runicitems.item.template.RunicItemTemplate;
+import com.runicrealms.runicitems.item.template.RunicItemWeaponTemplate;
 import com.runicrealms.runicitems.item.template.RunicRarityLevelItemTemplate;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LootManager {
 
     public static final HashMap<RunicItemRarity, Integer> RARITY_DROP_TABLE_WEIGHTS;
     public static final List<RunicItemRarity> RARITY_DROP_TABLE_WEIGHTED;
+
+    private static final List<RarityItemType> RARITY_ARMOR_WEAPON_DISTRIB_WEIGHTED;
 
     static {
         RARITY_DROP_TABLE_WEIGHTS = new HashMap<>();
@@ -24,8 +30,16 @@ public class LootManager {
                 RARITY_DROP_TABLE_WEIGHTED.add(rarity);
             }
         }
+
+        RARITY_ARMOR_WEAPON_DISTRIB_WEIGHTED = new ArrayList<>(5);
+        for (int i = 0; i < 4; i++) RARITY_ARMOR_WEAPON_DISTRIB_WEIGHTED.add(RarityItemType.ARMOR);
+        RARITY_ARMOR_WEAPON_DISTRIB_WEIGHTED.add(RarityItemType.WEAPON);
     }
-    private static final Map<Integer, Map<RunicItemRarity, List<RunicRarityLevelItemTemplate>>> rarityItems = new HashMap<>();
+    // Maps level to a mapping between rarities and a list of available templates
+    private static final Map<Integer, Map<RunicItemRarity, List<RunicRarityLevelItemTemplate>>> armorItems = new HashMap<>();
+    // This includes artifacts because artifact instanceof weapon
+    private static final Map<Integer, Map<RunicItemRarity, List<RunicRarityLevelItemTemplate>>> weaponItems = new HashMap<>();
+
     private static final Random random = new Random();
 
     public static RunicItemRarity rollRarity() {
@@ -37,9 +51,15 @@ public class LootManager {
             if (entry.getValue() instanceof RunicRarityLevelItemTemplate) {
                 if (entry.getKey().startsWith("script")) {
                     RunicRarityLevelItemTemplate template = (RunicRarityLevelItemTemplate) entry.getValue();
-                    if (!rarityItems.containsKey(template.getLevel())) {
-                        rarityItems.put(template.getLevel(), new HashMap<>());
+                    Map<Integer, Map<RunicItemRarity, List<RunicRarityLevelItemTemplate>>> rarityItems = null;
+                    if (entry.getValue() instanceof RunicItemArmorTemplate) {
+                        rarityItems = armorItems;
+                    } else if (entry.getValue() instanceof RunicItemWeaponTemplate) {
+                        rarityItems = weaponItems;
                     }
+                    if (rarityItems == null) continue;
+
+                    if (!rarityItems.containsKey(template.getLevel())) rarityItems.put(template.getLevel(), new HashMap<>());
                     if (!rarityItems.get(template.getLevel()).containsKey(template.getRarity())) {
                         rarityItems.get(template.getLevel()).put(template.getRarity(), new LinkedList<>());
                     }
@@ -50,19 +70,38 @@ public class LootManager {
     }
 
     public static List<RunicRarityLevelItemTemplate> getTemplatesInLevel(int level, RunicItemRarity rarity) {
-        if (!rarityItems.containsKey(level)) return new ArrayList<>();
-        return rarityItems.get(level).get(rarity);
+        if (!armorItems.containsKey(level) && !weaponItems.containsKey(level)) return new ArrayList<>();
+        if (!armorItems.containsKey(level)) return weaponItems.get(level).get(rarity);
+        if (!weaponItems.containsKey(level)) return armorItems.get(level).get(rarity);
+        return Stream.concat(
+                armorItems.get(level).get(rarity).stream(),
+                weaponItems.get(level).get(rarity).stream()).collect(Collectors.toList());
     }
 
     /**
      * Gets a random item in level range, INCLUSIVE.
-     * @param min - minimum level
+     * @param min - minimum levels
      * @param max - maximum level
      * @return random item
      */
     public static RunicRarityLevelItemTemplate getRandomItemInRange(int min, int max) {
         int amount = 0;
         RunicItemRarity rarity = rollRarity();
+
+        Map<Integer, Map<RunicItemRarity, List<RunicRarityLevelItemTemplate>>> rarityItems = null;
+        switch (RARITY_ARMOR_WEAPON_DISTRIB_WEIGHTED.get(random.nextInt(RARITY_ARMOR_WEAPON_DISTRIB_WEIGHTED.size()))) {
+            case ARMOR:
+                rarityItems = armorItems;
+                break;
+            case WEAPON:
+                rarityItems = weaponItems;
+                break;
+            default:
+                // Kotlin removes need for default because if you use a "when" case on an enum,
+                // you can apply the "sealed" keyword to make it exhaustive (values are checked during compile time, not run time)
+                throw new RuntimeException("Could not find script generated loot for levels " + min + "-" + max + " rarity " + rarity.getIdentifier().toUpperCase() + ".");
+        }
+
         for (int i = min; i <= max; i++) {
             if (rarityItems.containsKey(i)) {
                 if (rarityItems.get(i).containsKey(rarity)) amount += rarityItems.get(i).get(rarity).size();
@@ -79,6 +118,12 @@ public class LootManager {
             }
         }
         throw new RuntimeException("Could not find script generated loot for levels " + min + "-" + max + " rarity " + rarity.getIdentifier().toUpperCase() + ".");
+    }
+
+    private enum RarityItemType {
+
+        WEAPON, ARMOR;
+
     }
 
 }
